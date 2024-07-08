9ded4563d927f9bcfc003e6347223e9199eaea51
bbddihr <bbddihr@gmail.com>
2024-07-08 18:10:44 +0900

    form 데이터전송 -modelAndview

---
+++ p0708_04/src/main/java/com/java/Controller/FrontController.java
@@ -0,0 +1,95 @@
+package com.java.Controller;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class FrontController {
+	
+	// get, post 방식
+	@RequestMapping(value = "board/index", method =RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+	// get, post 방식 둘다 사용	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/member")  //회원가입폼
+	public String member() {
+		return "member";
+	}
+	
+	
+	
+	
+	@RequestMapping("/doMember") //회원가입
+	public ModelAndView doMember(HttpServletRequest request) {
+//		System.out.println("id: "+request.getParameter("id"));
+//		System.out.println("name: "+request.getParameter("name"));
+		String[] hobby =request.getParameterValues("hobby");
+		String hobbys = ""; //game,golf,run,swim,book
+		for(int i =0; i<hobby.length;i++) {
+			if(i==0) hobbys += ""+hobby[i];
+			else hobbys += ","+hobby[i];
+		}
+//		System.out.println("hobby:  " +Arrays.toString(hobby));
+		
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("doMember");  //파일이름
+		mv.addObject("id",request.getParameter("id"));
+		mv.addObject("pw",request.getParameter("pw"));
+		mv.addObject("name",request.getParameter("name"));
+		mv.addObject("phone",request.getParameter("phone"));
+		mv.addObject("gender",request.getParameter("gender"));
+		mv.addObject("hobby",hobbys);
+//		mv.addObject("hobbys",request.getParameterValues("hobby"));
+		
+		return mv;
+		
+		
+	}
+		@RequestMapping("/memUpdate") //회원정보수정
+		public String memUpdate(HttpServletRequest request) {
+			
+			String[] hobby =request.getParameterValues("hobby");
+			String hobbys = ""; //game,golf,run,swim,book
+			for(int i =0; i<hobby.length;i++) {
+				if(i==0) hobbys += ""+hobby[i];
+				else hobbys += ","+hobby[i];
+			}
+			System.out.println("hobby:  " +Arrays.toString(hobby));
+			
+			
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("doMember");  //파일이름
+			mv.addObject("id",request.getParameter("id"));
+			mv.addObject("pw",request.getParameter("pw"));
+			mv.addObject("name",request.getParameter("name"));
+			mv.addObject("phone",request.getParameter("phone"));
+			mv.addObject("gender",request.getParameter("gender"));
+			mv.addObject("hobby",hobbys);
+			
+			
+			
+			
+			return "memUpdate";
+		}
+	
+	
+	
+}
---
+++ p0708_04/src/main/webapp/WEB-INF/views/memUpdate.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>회원정보수정</title>
+	</head>
+	<body>
+	<h2>회원정보수정</h2>
+	<form action="doMember" name="frm" method="post">
+	<label>아이디</label>
+	<input type="text" name="id" value="${id }><br>
+	<label>패스워드</label>
+	<input type="text" name="pw" value="${pw }><br>
+	<label>이름</label>
+	<input type="text" name="name"><br>
+	<label>전화번호</label>
+	<input type="text" name="phone"><br>
+	<label>성별</label><br>
+	<input type="radio" name="gender" value="male" id="male">
+	<label for="male">남자</label>
+	<input type="radio" name="gender" value="female" id="female">
+	<label for="male">여자</label>
+	<br>
+	<label>취미</label><br>
+	<input type="checkbox" name="hobby" value="game" id="game">
+	<label for="game">게임</label>
+	<input type="checkbox" name="hobby" value="golf" id="golf">
+	<label for="game">골프</label>
+	<input type="checkbox" name="hobby" value="run" id="run">
+	<label for="game">조깅</label>
+	<input type="checkbox" name="hobby" value="swim" id="swim">
+	<label for="game">수영</label>
+	<input type="checkbox" name="hobby" value="book" id="book">
+	<label for="game">독서</label>
+	<br>
+	<input type="submit" value="회원가입">
+
+	</form>
+	</body>
+	
+</html>
